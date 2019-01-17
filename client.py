c1e12081173643bf4b3191055398c8bebfd5b791
pakichu <smolguppy@gmail.com>
2019-01-17 08:43:28 -0600

    fix

--- client.py
+++ client.py
@@ -4,16 +4,16 @@ import time
  
 s = socket.socket()
 host = input(str("Please enter the hostname of the server : "))
-port = 8080
+port = 8000
 s.connect((host,port))
 print(" Connected to chat server")
 while 1:
-            incoming_message = s.recv(1024)
-            incoming_message = incoming_message.decode()
-            print(" Server : ", incoming_message)
-            print("")
             message = input(str(">> "))
             message = message.encode()
             s.send(message)
             print("message has been sent...")
+            print("")
+            incoming_message = s.recv(1024)
+            incoming_message = incoming_message.decode()
+            print(" Server : ", incoming_message)
             print("")
