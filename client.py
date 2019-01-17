ba431ba46f9a7b530de1aeacd08d6b32b973f527
pakichu <smolguppy@gmail.com>
2019-01-17 12:58:29 -0600

    finished

--- client.py
+++ client.py
@@ -1,19 +1,28 @@
 import socket
 import sys
 import time
+import threading
  
 s = socket.socket()
 host = input(str("Please enter the hostname of the server : "))
 port = 8000
 s.connect((host,port))
 print(" Connected to chat server")
-while 1:
-            message = input(str(">> "))
-            message = message.encode()
-            s.send(message)
-            print("message has been sent...")
-            print("")
-            incoming_message = s.recv(1024)
-            incoming_message = incoming_message.decode()
-            print(" Server : ", incoming_message)
-            print("")
+def send_Messages():
+    while 1:
+        message = input(str(">>"))
+        message = message.encode()
+        s.send(message)
+        print("message has been sent...")
+        print("")
+def receive_Messages():
+    while True:
+        incoming_message = s.recv(1024)
+        incoming_message = incoming_message.decode()
+        print(" Server: ", incoming_message)
+        print("")
+
+incoming_thread = threading.Thread(target=receive_Messages)
+incoming_thread.start()
+send_thread = threading.Thread(target=send_Messages)
+send_thread.start()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import time
+import threading
 
 s = socket.socket()
 host = socket.gethostname()
@@ -10,11 +11,19 @@ s.bind((host,port))
 s.listen(1)
 conn, addr = s.accept()
 print(addr, "has connected")
-while 1:
-    message = input(">> ")
-    message = message.encode()
-    conn.send(message)
-    print("Message sent")
-    incoming_message = conn.recv(1024)
-    incoming_message = incoming_message.decode()
-    print(">> Client: ", message)
+def send_Messages():
+    while 1:
+        message = input(">>")
+        message = message.encode()
+        conn.send(message)
+        print("Message sent")
+def receive_Messages():
+    while True:
+        incoming_message = conn.recv(1024)
+        incoming_message = incoming_message.decode()
+        print(">> Client: ", incoming_message)
+
+incoming_thread = threading.Thread(target=receive_Messages)
+incoming_thread.start()
+send_thread = threading.Thread(target=send_Messages)
+send_thread.start()
